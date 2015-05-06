e76a78ddcb02cf9de2962c40fd3c6c8674d44879
Eder Juarez <juarezm0790@gmai.com>
2015-05-06 18:40:40 +0000

    mensaje

--- ordencliente/admin.py
+++
@@ -1,7 +0,0 @@
-from ordencliente.models import *
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(clientes)
-admin.site.register(servicios)
-admin.site.register(historial)
